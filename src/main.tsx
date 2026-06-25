e1cfa9bacd5cf70be577599f915c7ffa0188b3a3
moistbobo <moistbobo@gmail.com>
2026-06-25 22:13:36 +0800

    Revert "feat: add Vercel Analytics (#58)"
    This reverts commit cae1b056b22f450d5456c396ab42f68eb2e9f02b.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Analytics } from '@vercel/analytics/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import './i18n';
@@ -17,7 +16,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <ThemeProvider>
         <WorldsPreferencesProvider>
           <App />
-          <Analytics />
         </WorldsPreferencesProvider>
       </ThemeProvider>
       <ReactQueryDevtools initialIsOpen={false} />
