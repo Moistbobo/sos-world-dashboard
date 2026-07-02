ac0eff7194cff521907f0f97ec9cd879cf52cc70
moistbobo <moistbobo@gmail.com>
2026-07-02 20:43:55 +0800

    test(worlds-page): remove unused useFilterCounts import

--- src/pages/worlds/WorldsPage.test.tsx
+++ src/pages/worlds/WorldsPage.test.tsx
@@ -4,7 +4,6 @@ import userEvent from '@testing-library/user-event';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router-dom';
 import { WorldsPage } from './WorldsPage';
-import { useFilterCounts } from '../../hooks/useApi';
 import { WorldsPreferencesProvider } from '../../contexts/WorldsPreferencesContext';
 import { ListsProvider } from '../../contexts/ListsContext';
 
